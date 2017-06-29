f1724d5a09a20b0ffe64af93ab6faf318eb075bd
Jasper King-Harman <j.kingharman@googlemail.com>
2017-06-29 15:18:19 +0100

    Steps toward having multi multi tags

--- app/app.rb
+++ app/app.rb
@@ -7,39 +7,38 @@ require './app/data_mapper_setup.rb'
 
 
 class BookmarkManager < Sinatra::Base
-  
+
   get '/links' do
     @links = Link.all
     @tags = Tag.all
     erb :links
   end
-  
+
   before do
     @links = Link.all
     @tags = Tag.all
   end
-  
+
   get '/links/new' do
     erb :new_bookmark
   end
-  
+
   post '/links' do
     link = Link.new(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
-    link.save
-    redirect '/links'
+    tags = params[:tags].split(",")
+    tags.each do |tag_name|
+      tag = Tag.create(name: tag_name)
+      link.tags << tag
+      link.save
+      redirect '/links'
+    end
   end
-  
-  
-  
+
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :links
   end
-  
-  
-  
+
   run! if app_file == $0
 end
--- spec/adding_tag_to_link_spec.rb
+++ spec/adding_tag_to_link_spec.rb
@@ -5,8 +5,21 @@ feature 'users can add a tag' do
     fill_in 'title', with: 'gardenersworld'
     fill_in 'tags', with: 'gardening'
     click_button 'Create link'
-    
+
     link = Link.first
     expect(link.tags.map(&:name)).to include('gardening')
+  end
+end
+
+feature 'users can add multiple tags' do
+  scenario 'user adds gardening and business tag' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.gardenersworld.com/'
+    fill_in 'title', with: 'gardenersworld'
+    fill_in 'tags', with: 'gardening, leisure'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('gardening')
+    expect(link.tags.map(&:name)).to include('leisure')
   end
 end
