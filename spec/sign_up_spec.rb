24b78a3c36a0f4f49d47e479ddfca10d05089ac3
terminalobject <doctorboredom@hotmail.com>
2017-06-30 13:00:14 +0100

    safe commit

--- spec/sign_up_spec.rb
+++ spec/sign_up_spec.rb
@@ -1,7 +1,8 @@
 feature "Sign up page" do
 scenario "user visits url and inputs details, gets personalised confirmation" do
   visit ('/users/new')
-  fill_in 'email', with: 'joe@hotmail.com'
+  expect(page).to have_content 'hello'
+  fill_in 'example', with: 'joe@hotmail.com'
   fill_in 'password', with: 'kettlefish129'
   click_button 'sign-up'
   expect(page).to have_content 'joe@hotmail.com'
